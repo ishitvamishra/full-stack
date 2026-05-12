f796a1095406a3f14eabdcedd33897cf9da45110
Ishitva <ishitvamishra85@gmail.com>
2026-05-13 05:17:44 +0530

    Adding new files

--- Frontend/javascript/10_oops/01_oop.js
+++ Frontend/javascript/10_oops/01_oop.js
@@ -1,27 +1,27 @@
-// //an object having properties and methods is called an object literal.
-// const user = {
-//     username: "ishitva",
-//     loginCount: 8,
-//     signedIn: true,
+//an object having properties and methods is called an object literal.
+const user = {
+    username: "ishitva",
+    loginCount: 8,
+    signedIn: true,
 
-//     getUserDetails: function(){
-//         console.log("Got user details from database");
-//         //this is used with username to tell which username we are referring to, because there can be multiple username variables in the code, and we want to refer to the one that is inside the user object.
-//         console.log(`Username: ${this.username}`);
-//         //here this is used to give the current context of the object, which is user in this case.
-//         console.log(this);
-//     }
+    getUserDetails: function(){
+        console.log("Got user details from database");
+        //this is used with username to tell which username we are referring to, because there can be multiple username variables in the code, and we want to refer to the one that is inside the user object.
+        console.log(`Username: ${this.username}`);
+        //here this is used to give the current context of the object, which is user in this case.
+        console.log(this);
+    }
 
-// }
+}
 
 
 
-// console.log(user.username) //return ishitva 
-// console.log(user.getUserDetails());
-//  //returns Got user details from database
-//  //Username: ishitva
-//  //{username: 'ishitva', loginCount: 8, signedIn: true, getUserDetails: [Function: getUserDetails]}
-// console.log(this);
+console.log(user.username) //return ishitva 
+console.log(user.getUserDetails());
+ //returns Got user details from database
+ //Username: ishitva
+ //{username: 'ishitva', loginCount: 8, signedIn: true, getUserDetails: [Function: getUserDetails]}
+console.log(this);
 
 
 function User(username, loginCount, isLoggedIn){
@@ -43,6 +43,6 @@ const userOne = new User("hitesh", 12, true)
 const userTwo = new User("ChaiAurCode", 11, false)
 //constructor here refers to the function that was used to create the instance of the object, which is User in this case. So when we log userOne.constructor, it will return the User function.
 
-// console.log(userOne.constructor);
-// console.log(userTwo);
+console.log(userOne.constructor);
+console.log(userTwo);
 console.log(userOne.greeting());
